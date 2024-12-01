fca2897b1a521c9d01aaeaea8e1a6eedeff55c90
AbdulazizRus <coderdim@gmail.com>
2024-12-01 22:08:02 +0500

    resgister endpoint added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 // Connect to MongoDB
 connectToDB();
@@ -10,9 +11,13 @@ connectToDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Routes
+app.use('/api/users', userRoutes);
 
 module.exports = app;
