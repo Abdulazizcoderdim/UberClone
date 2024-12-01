66121f576ca5bb6d7d832424c1774a45c9b99f4b
AbdulazizRus <coderdim@gmail.com>
2024-12-01 18:28:34 +0500

    register endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,5 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
 
 module.exports.registerUser = async (req, res, next) => {};
