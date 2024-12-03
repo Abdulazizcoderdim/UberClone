c77bf3060a29f192138220441192c23cb177a82a
AbdulazizRus <coderdim@gmail.com>
2024-12-03 18:53:23 +0500

    token cokkies

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const connectToDB = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser');
 
 // Connect to MongoDB
 connectToDB();
@@ -12,6 +13,7 @@ connectToDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
